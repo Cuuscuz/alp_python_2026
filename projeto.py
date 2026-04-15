1bc272facd3aec74daa96facea2fda6d2670d6e2
Cuscuz <alisson.c0scuz@gmail.com>
2026-04-15 20:04:21 -0300

    Add files via upload

---
+++ projeto.py
@@ -0,0 +1,7 @@
+
+base = int(input("Informe a base do retângulo:"))
+altura = int(input("Agora informe a altura do mesmo retângulo:"))
+
+area = base * altura 
+
+print(f"Ok, o seu retângulo tem a aréa de: {area}")
