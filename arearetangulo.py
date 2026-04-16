d193f753a4ea285ad5fe6b0dd78f80d2b6231d6a
Cuuscuz <alisson.c0scuz@gmail.com>
2026-04-16 09:47:29 -0300

    area

---
+++ arearetangulo.py
@@ -0,0 +1,7 @@
+
+base = int(input("Informe a Base o ratângulo, em centimetros:"))
+altura = int(input("Agora informe a Altura, também em centimetros:"))
+
+area = base * altura
+
+print(f"A aréa do retângulo é {area}cm²")
