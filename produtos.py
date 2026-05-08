b54f87921af6731fda0ab95a683705fbe00cdfb9
Cuuscuz <alisson.c0scuz@gmail.com>
2026-05-08 10:58:24 -0300

    atividade em sala

---
+++ produtos.py
@@ -0,0 +1,9 @@
+
+print("Preencha as informações do produto a seguir:")
+
+preco = float(input("Digite o preço do produto:"))
+quant = int(input("Agora digite o quantidade que foi comprada deste produto:"))
+
+total = preco * quant
+
+print(f"O valor total da compra é de R${total}.")
