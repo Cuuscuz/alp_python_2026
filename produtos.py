ea0407cbf4a7776a82b0ccc38051a932e87a619c
Cuuscuz <alisson.c0scuz@gmail.com>
2026-05-08 11:04:39 -0300

    atividade em sala: Preço de produtos

--- produtos.py
+++ produtos.py
@@ -2,8 +2,10 @@
 print("Preencha as informações do produto a seguir:")
 
 preco = float(input("Digite o preço do produto:"))
-quant = int(input("Agora digite o quantidade que foi comprada deste produto:"))
+if preco <= 0:
+    print("Digite o preço de um produto real e válido.")
 
-total = preco * quant
-
-print(f"O valor total da compra é de R${total}.")
+else:
+    quant = int(input("Agora digite o quantidade que foi comprada deste produto:"))
+    total = preco * quant
+    print(f"O valor total da compra é de R${total}.")
