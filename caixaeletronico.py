1acf4bdc6d7e69bc9180e49f2e7848b014c35418
Cuuscuz <alisson.c0scuz@gmail.com>
2026-05-28 10:57:00 -0300

    caixa eletronico

---
+++ caixaeletronico.py
@@ -0,0 +1,18 @@
+
+senha = int(input("Olá, digite a senha: "))
+tent = 3
+
+while tent != 0:
+    if senha == 123456:
+        print("Olá, ALISSON. Seja bem vindo ao nosso banco!")
+        break
+        
+    else: 
+        if tent > 1:
+            tent -= 1
+            print(f"Senha errada, você ainda tem {tent} tentativas!")
+            senha = int(input("Digite novamente sua senha: "))
+
+        else:
+            print("Sua senha foi bloqueada! Vá até alguma agência para desbloquear.")
+            break
