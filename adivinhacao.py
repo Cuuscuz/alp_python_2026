0d774ad8fa0534fedcf6b9f2d1f5c289adb7ebce
Cuuscuz <alisson.c0scuz@gmail.com>
2026-06-11 09:48:23 -0300

    modificação do jogo.

--- adivinhacao.py
+++ adivinhacao.py
@@ -2,27 +2,22 @@ from random import randint
 
 num_sort = randint(1,10)
 tent = 0
+acerto = False
 
-while tent != 3:
-    num = int(input("Escolha um número de 1 à 10: "))
+while tent != 3 and not acerto:
+    num = int(input("Digite um número de 1 à 10:"))
 
-    if num < num_sort:
-        tent += 1
-        if tent == 3: 
-            break
-        else:
-            print("Você errou, tente um número maior!")
+    if num == num_sort:
+        acerto = True 
+        print("Parabéns, você acertou!!!")
 
     elif num > num_sort:
         tent += 1
-        if tent == 3:
-            break
-        else:
-            print("Você errou, tente um número menor!")
+        print("Você errou, tente um número menor!")
 
-    else:
-        print("Parabéns, você acertou!")
-        break
+    elif num < num_sort:
+        tent += 1
+        print("Você errou, tente um número maior!")
 
 if tent == 3:
-    print("Errou novamente! Limite de tentativas encerrados")
+    print("Você já tenou 3 vezes, programa encerrado!!")
